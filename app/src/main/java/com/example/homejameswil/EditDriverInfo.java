5fbe372d311b0ef7141df7470e7972a17cdd8902
David Britnor <ST10118530@vcconnect.edu.za>
2022-11-23 18:50:51 +0200

    Jared

--- app/src/main/java/com/example/homejameswil/EditDriverInfo.java
+++ app/src/main/java/com/example/homejameswil/EditDriverInfo.java
@@ -1,5 +1,6 @@
 package com.example.homejameswil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,16 +11,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class EditDriverInfo extends Fragment
@@ -29,6 +36,7 @@ public class EditDriverInfo extends Fragment
     String TAG = "Firebase";
     FirebaseUser user;
     ImageButton btnBack;
+    Button btnSave;
     View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +45,7 @@ public class EditDriverInfo extends Fragment
         view = inflater.inflate(R.layout.fragment_edit_driver_info, container, false);
 
         retrieveDriverInfo();
+        EditInfo();
         GoBack();
 
         return view;
@@ -115,5 +124,61 @@ public class EditDriverInfo extends Fragment
                 }
             }
         });
+    }
+
+    private void EditInfo()
+    {
+        Bundle b = getArguments();
+        String docuid = b.getString("docID");
+        btnSave = view.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                //Get logged in users email
+                String email = user.getEmail();
+                Map<String, Object> user = new HashMap<>();
+                //Map<String, Object> userStatus = new HashMap<>();
+
+                user.put("Email", email);
+                user.put("Status", "Driver");
+                user.put("UserName", txtName.getText().toString());
+                user.put("UserSurname", txtSurname.getText().toString());
+                user.put("UserIDNumber", txtIDNumber.getText().toString());
+                user.put("UserCellNumber", txtCellNumber.getText().toString());
+                user.put("VehicleType", txtVehType.getText().toString());
+                user.put("Colour", txtColour.getText().toString());
+                user.put("VehicleNumPlate", txtVehNumPlate.getText().toString());
+
+                db.collection("Users").document(docuid)
+                        .update(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot added with ID:");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+                Bundle bundle = new Bundle();
+                bundle.putString("docID", docuid);
+                Fragment fragment = new AccountDetailsDriver();
+                fragment.setArguments(bundle);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.frameLayout, fragment);
+                ft.commit();
+            }
+        });
+
+
+
     }
 }
